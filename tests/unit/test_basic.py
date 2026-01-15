a945a16cdfe6a9f1d5e8cdb7be20c3defabb725d
Christophe De Troyer <christophe.detroyer@gmail.com>
2026-01-15 15:34:09 +0100

    initial

---
+++ tests/unit/test_basic.py
@@ -0,0 +1,10 @@
+"""Unit tests for m1dnight.infra."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    assert bool(1) is True
